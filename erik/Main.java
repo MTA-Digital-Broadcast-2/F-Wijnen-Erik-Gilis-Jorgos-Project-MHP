ab9dbe0c6a9f8c7cc0c593c5c66b77fac3fa4353
ErikWijnen <wijnen.erik1990@gmail.com>
2015-02-15 22:15:45 +0100

    Main class en al eerste foto's
    Ik ben gestart met woorden die moeten worden weergegeven en de foto's

---
+++ erik/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package killkitten;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+public class Main {
+   
+    
+    public static void main(String[] args) {
+          java.lang.String alleWoorden[]={
+          "aging","lover","posen","knack",
+          "horny","miffy","nic","liana",
+          "surgy","gelid","euryclea","champion",
+          "bondmaid","returnee","orogenic","metacenter",
+          "exfoliated","anastomose","illiteracy","amerindian",
+          "unexplainable","nosogeography","electroplaque",
+          "affreightment","reconvergence"};
+          
+          Scanner keyboard = new Scanner(System.in);
+          System.out.println("enter word ");
+          java.lang.String myChar = keyboard.next();
+          
+          
+        /*for(int i=0;i<alleWoorden.length;i++){
+            System.out.println(alleWoorden[i]);*/
+          System.out.println(myChar);
+                  
+         if(myChar == "bla")
+         {
+             System.out.println("wooop woop");
+         }
+         }
+        }
+    
+
+
